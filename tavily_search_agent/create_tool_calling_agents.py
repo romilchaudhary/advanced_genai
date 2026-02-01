dd35c876b6caa03a15b03d93abef9f3118cda875
romilchaudhary <romil.roar@gmail.com>
2026-02-01 08:08:25 +0530

    added create_tool_calling_agent logic

--- tavily_search_agent/create_tool_calling_agents.py
+++ tavily_search_agent/create_tool_calling_agents.py
@@ -2,79 +2,44 @@ from dotenv import load_dotenv
 load_dotenv()
 
 from langchain_ollama import ChatOllama
-from langchain.agents import create_agent
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_classic.agents import create_tool_calling_agent, AgentExecutor
 from langchain.tools import tool
-from langchain_community.tools import WikipediaQueryRun
-from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_core.prompts import ChatPromptTemplate
 
-from langchain_core.messages import AIMessage
+@tool("calculator_tool", description="perform mathematical operations, use this for any math problem")
+def calculator(expression: str)-> str:
+    """ funcction to use to calculate mathematical expressions """
+    print(f"[TOOL CALLED] calculator with input: {expression}")
+    return str(eval(expression))
+
+tools = [calculator]
 
 llm = ChatOllama(
     model="mistral:7b-instruct",
     temperature=0
-)
-
-def extract_final_answer(messages):
-    for msg in messages:
-        if isinstance(msg, AIMessage) and msg.content.strip():
-            return msg.content
-    return None
-
-@tool
-def calculator(expression: str) -> str:
-    """Use this tool for mathematical calculations."""
-    try:
-        print(f"[TOOL CALLED] calculator with input: {expression}")
-        return str(eval(expression))
-    except:
-        return "Invalid expression"
-
-wiki_tool = WikipediaQueryRun(
-    api_wrapper=WikipediaAPIWrapper(
-        top_k_results=1,
-        doc_content_chars_max=300
-    )
-)
-
-tools = [calculator, wiki_tool]
+).bind_tools(tools)
 
 prompt = ChatPromptTemplate.from_messages([
-    ("system",
-    "You MUST use a tool before answering. "
-    "If math is involved, use calculator. "
-    "If facts are asked, use Wikipedia. "
-    "End with FINAL ANSWER."),
+    ("system", "You are a helpful AI agent. Use tools when needed."),
     ("human", "{input}"),
-    ("assistant", "{agent_scratchpad}")
+    ("placeholder", "{agent_scratchpad}")
 ])
 
-agent = create_agent(
-    model=llm,
+agent = create_tool_calling_agent(
+    llm = llm,
+    tools = tools,
+    prompt = prompt
+)
+
+agent_executor = AgentExecutor(
+    agent=agent,
     tools=tools,
-    system_prompt="You are a helpful assistant, use tools to answer whenever required and response the final answer"
+    verbose=True,
+    handle_parsing_errors=True
 )
 
-result = agent.invoke(
-    {
-        "messages":
-            [
-                {
-                    "role": "user",
-                    "content": "What is the value of 10+2+(2*3)+(4/2)?"
-                }
-            ]
-    }
-)
-print("Answer: ", extract_final_answer(result["messages"][::-1]))
-
-result = agent.invoke({
-    "messages": [
-        {
-            "role": "user",
-            "content": "When was Tata Group founded?"
-        }
-    ]
+result = agent_executor.invoke({
+    "input": "What is 10 + 2 + (2 * 3) + (4 / 2)?"
 })
 
-print("Answer: ", extract_final_answer(result["messages"][::-1]))
+print(result)
